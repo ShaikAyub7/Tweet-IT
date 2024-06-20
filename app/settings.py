8afa24e55dbb3acdd1abcc22d5c24d15377b6c08
ShaikAyub7 <skayub929@gmail.com>
2024-06-20 18:41:08 +0530

    static files

--- app/settings.py
+++ app/settings.py
@@ -164,3 +164,6 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
