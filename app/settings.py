36ddf7ad4fa6dbe699c38ebf84a7ffe18945afb8
ShaikAyub7 <skayub929@gmail.com>
2024-06-20 17:46:09 +0530

    database

--- app/settings.py
+++ app/settings.py
@@ -77,12 +77,25 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'DmeEOhBNwsWnNEbuyuNxEARcwkCLxiVa',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '41435',
     }
 }
+
 # DATABASES["default"] = dj_database_url.parse("postgres://tweet_it_user:UAdPgzcVgubs38x273pYo9vWHtpXhyb5@dpg-cpopjq2ju9rs738tmc80-a.oregon-postgres.render.com/tweet_it")
 
 # postgres://tweet_it_user:UAdPgzcVgubs38x273pYo9vWHtpXhyb5@dpg-cpopjq2ju9rs738tmc80-a.oregon-postgres.render.com/tweet_it
